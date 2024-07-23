5ac6251c00c0d593d9acd2bc989b05821bbb36d2
Harry CAO <V176HAC@cathaypacific.com>
2024-07-23 18:53:43 +0800

    start building fade in animation

---
+++ Cathay-Animation-Demo/Result/ResultModel.swift
@@ -0,0 +1,12 @@
+//
+//  ResultModel.swift
+//  Cathay-Animation-Demo
+//
+//  Created by harry.weixian.cao on 2024/7/23.
+//
+
+import Foundation
+
+struct ResultModel {
+    var num: Int?
+}
--- Cathay-Animation-Demo/Result/ResultViewController.swift
+++ Cathay-Animation-Demo/Result/ResultViewController.swift
@@ -9,7 +9,13 @@ import UIKit
 import SnapKit
 
 class ResultViewController: UIViewController {
-    var dataSource: [Int]?
+    var dataSource: [ResultModel] = {
+        var data = [ResultModel]()
+        for i in 0...20 {
+            data.append(ResultModel())
+        }
+        return data
+    }()
     private let headerView: UIImageView = {
         let imageView = UIImageView()
         imageView.frame.size.height = UIScreen.main.bounds.height
@@ -43,9 +49,11 @@ class ResultViewController: UIViewController {
         tableView.isScrollEnabled = false
         MockNetworkHelper.mockRequestData { [weak self] data in
             guard let self = self else { return }
-            dataSource = data
+            dataSource = data.map{ ResultModel(num: $0) }
             tableView.isScrollEnabled = true
-            tableView.reloadData()
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+                self.tableView.reloadRows(at: self.dataSource.map{ IndexPath(row: $0.num!, section: 0) }, with: .none)
+            }
             UIView.animate(withDuration: 0.3) { [self] in
                 self.headerView.frame.size.height = 200
                 self.headerView.layer.masksToBounds = true
@@ -58,11 +66,12 @@ class ResultViewController: UIViewController {
 
 extension ResultViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataSource?.count ?? 10
+        print(dataSource.count)
+        return dataSource.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let _ = dataSource?[indexPath.row] else {
+        guard let _ = dataSource[indexPath.row].num else {
             let cell = UITableViewCell()
             cell.backgroundColor = .yellow
             return cell
