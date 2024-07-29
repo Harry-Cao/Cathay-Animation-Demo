7aa0a133d7322393b32c6c4d58a6b055e8734e34
Harry Cao <V176HAC@cathaypacific.com>
2024-07-29 18:53:35 +0800

    start building TabView

---
+++ Cathay-Animation-Demo/TabView/TabItem.swift
@@ -0,0 +1,16 @@
+//
+//  TabItem.swift
+//  Cathay-Animation-Demo
+//
+//  Created by harry.weixian.cao on 2024/7/29.
+//
+
+import Foundation
+
+class TabItem {
+    let title: String
+
+    init(title: String) {
+        self.title = title
+    }
+}
--- Cathay-Animation-Demo/TabView/TabView.swift
+++ Cathay-Animation-Demo/TabView/TabView.swift
@@ -6,8 +6,17 @@
 //
 
 import UIKit
+import SnapKit
 
 class TabView: UIView {
+    private var dataSource: [TabItem] = [
+        TabItem(title: "1")
+    ]
+    private lazy var tabContainer: UIScrollView = {
+        let view = UIScrollView()
+        return view
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .yellow
@@ -15,5 +24,12 @@ class TabView: UIView {
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupUI() {
+        [tabContainer].forEach(addSubview)
+        tabContainer.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
